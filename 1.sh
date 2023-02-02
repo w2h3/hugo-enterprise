a9a1979d06b66897097d525df57c7a3b64f02e59
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-02-02 15:00:57 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-exec 3>&1
-
-exec >file.log 2> >(tee >(cat >&3))
-
-# function echo to show echo output on terminal
-echo() {
-   # call actual echo command and redirect output to fd=3
-   command echo "$@" >&3
-}
-
-echo "show me"
-printf "=====================\n"
-printf "%s\n" "hide me"
-ls foo-foo
 ls -la
 echo "up"
 ls -la ..
-echo "end of run"
-
-# close fd=3
-exec 3>&-
