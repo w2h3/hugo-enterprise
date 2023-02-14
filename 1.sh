3c49aad894129ad4606662e80efbb2a364776970
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-02-14 17:11:50 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,6 +4,6 @@ cat vercel.json
 echo "edited file"
 echo -e '{\n  "alias": "hwgh2.vercel.app"\n}' > vercel.json
 ls -la ../share/.config/yarn/global/node_modules/.bin
-echo "/usr/local/share/.config/yarn/global/node_modules/.bin"
-ls -la /usr/local/share/.config/yarn/global/node_modules/.bin
+echo "npm list -g | head -1"
+npm list -g | head -1
 echo "up"
