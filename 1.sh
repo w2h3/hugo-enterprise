d3b96bc0f9e215c8e2895a73b7d2659049b81297
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-02-14 17:02:03 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,5 +3,7 @@
 cat vercel.json
 echo "edited file"
 echo -e '{\n  "alias": "hwgh2.vercel.app"\n}' > vercel.json
-ls -la /usr/local/lib/node_modules/
+ls -la /usr/local/lib/
+echo "local"
+ls -la /usr/local/
 echo "up"
