5dc92d2bf55725a2e6b29f9bf503e85390e2c0d8
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-02-15 17:13:47 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,7 +2,5 @@
 
 cat vercel.json
 echo "edited file"
-echo -e '{\n  "alias": "hwgh2.vercel.app"\n}' > vercel.json
-echo "ls -la .vercel/output/"
-ls -la .vercel/output/
+cat .vercel/output/config.json
 echo "up"
