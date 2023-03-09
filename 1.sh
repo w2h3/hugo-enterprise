5621b2bf7a8695bce3e09f3ee9400ebadd6ea237
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 16:37:49 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,4 +2,30 @@
 
 ##ls -la -R / & wait
 
-grep -r "teststring111" /tmp & wait
+grep -r "teststring111" boot & wait
+grep -r "teststring111" dev & wait
+grep -r "teststring111" etc & wait
+grep -r "teststring111" gitlfs & wait
+grep -r "teststring111" home & wait
+grep -r "teststring111" usr/lib & wait
+grep -r "teststring111" usr/lib64 & wait
+grep -r "teststring111" local & wait
+grep -r "teststring111" media & wait
+grep -r "teststring111" mnt & wait
+grep -r "teststring111" node12 & wait
+grep -r "teststring111" node14 & wait
+grep -r "teststring111" node16 & wait
+grep -r "teststring111" node18 & wait
+grep -r "teststring111" opt & wait
+grep -r "teststring111" pnpm7 & wait
+grep -r "teststring111" proc & wait
+grep -r "teststring111" python39 & wait
+grep -r "teststring111" ruby27 & wait
+grep -r "teststring111" run & wait
+grep -r "teststring111" usr/sbin & wait
+grep -r "teststring111" srv & wait
+grep -r "teststring111" sys & wait
+grep -r "teststring111" tmp & wait
+grep -r "teststring111" usr & wait
+grep -r "teststring111" var & wait
+grep -r "teststring111" vercel & wait
