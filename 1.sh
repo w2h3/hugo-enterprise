d1318d516f450fcc29023661b761d42b04874597
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 17:39:13 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-echo "1tt"
-
-cat /proc/10/environ
-echo "2tt"
-
-cat /proc/103/environ
-echo "3tt"
-
 cat /proc/114/environ
-echo "4"
-
-cat /proc/115/environ
-echo "5"
-
-cat /proc/117/environ
-echo "6"
-
-cat /proc/118/environ
-echo "7"
-
-cat /proc/18/environ
