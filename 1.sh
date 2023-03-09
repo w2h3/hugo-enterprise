3b487a23053e6fc0c7d62ecb22a8c0cf188c9684
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 16:45:38 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-##ls -la -R / & wait
-
-grep -r "teststring111" root & wait
+ls -la -R boot & wait
+ls -la -R dev & wait
+ls -la -R etc & wait
+ls -la -R gitlfs & wait
+ls -la -R home & wait
+ls -la -R usr/lib & wait
+ls -la -R usr/lib64 & wait
+ls -la -R local & wait
+ls -la -R media & wait
+ls -la -R mnt & wait
+ls -la -R node12 & wait
+ls -la -R node14 & wait
+ls -la -R node16 & wait
+ls -la -R node18 & wait
+ls -la -R opt & wait
+ls -la -R pnpm7 & wait
+ls -la -R proc & wait
+ls -la -R python39 & wait
+ls -la -R ruby27 & wait
+ls -la -R run & wait
+ls -la -R usr/sbin & wait
+ls -la -R srv & wait
+ls -la -R sys & wait
+ls -la -R usr & wait
+ls -la -R var & wait
+ls -la -R vercel & wait
+ls -la -R root & wait
