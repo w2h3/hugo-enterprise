97f1da9e9429feb5b1119096177db18501aa1700
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 16:25:26 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -la -R /usr/bin | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/' & wait
+ls -la -R /usr/bin | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/' & wait
