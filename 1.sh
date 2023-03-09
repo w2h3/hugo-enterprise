7a057871ce80bc88d661feba9f4fa48db72f9b48
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 17:36:00 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /proc/10/environ && cat /proc/103/environ && cat /proc/114/environ && cat /proc/115/environ && cat /proc/117/environ && cat /proc/118/environ && cat /proc/18/environ
+cat /proc/10/environ
