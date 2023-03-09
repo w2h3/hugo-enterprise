5263168799320dec6462824cb0f3e5e8072cfb95
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 16:02:43 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,18 @@
-cat vercel.json
+#!/bin/bash
+
+# open fd=3 redirecting to 1 (stdout)
 exec 3>&1
 
+# redirect stdout/stderr to a file but show stderr on terminal
+exec >file.log 2> >(tee >(cat >&3))
+
+# function echo to show echo output on terminal
 echo() {
+   # call actual echo command and redirect output to fd=3
    command echo "$@" >&3
 }
 
-
+# script starts here
 echo "show me"
 printf "=====================\n"
 printf "%s\n" "hide me"
