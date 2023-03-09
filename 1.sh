434cf003215b109ce7a60050f363f16e9ed89f8c
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-09 16:50:54 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
-grep -r "test" /boot & wait
-grep -r "test" /dev & wait
-grep -r "test" /etc & wait
-grep -r "test" /gitlfs & wait
-grep -r "test" /home & wait
-grep -r "test" /usr/lib & wait
-grep -r "test" /usr/lib64 & wait
-grep -r "test" /local & wait
-grep -r "test" /media & wait
-grep -r "test" /mnt & wait
-grep -r "test" /node12 & wait
-grep -r "test" /node14 & wait
-grep -r "test" /node16 & wait
-grep -r "test" /node18 & wait
-grep -r "test" /opt & wait
-grep -r "test" /pnpm7 & wait
-grep -r "test" /proc & wait
-grep -r "test" /python39 & wait
-grep -r "test" /ruby27 & wait
-grep -r "test" /run & wait
-grep -r "test" /usr/sbin & wait
-grep -r "test" /srv & wait
-grep -r "test" /sys & wait
 grep -r "test" /tmp & wait
-grep -r "test" /usr & wait
-grep -r "test" /var & wait
-grep -r "test" /vercel & wait
-1
