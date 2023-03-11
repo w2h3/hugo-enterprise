893d9e3fefbbb1356891bceec6caeba2c7f7d49e
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-11 16:38:20 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI=\(.*\)FLUENT_PORT=.*$/\1/p')
+string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI=\(.*\)AWS_DEFAULT_REGION=.*$/\1/p')
 string2="/containers/build-fargate-container-main"
 string3="$string$string2"
 echo "$string3"
