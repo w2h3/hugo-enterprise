9d446e04daa1d8c07a7c7867ce4b4c56cd7a1145
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-11 16:11:47 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI_V4=\([^[:space:]]*\)FLUENT_PORT=.*[^[:space:]]*$/\1/p')
-echo "$string" + "ccc"
+string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI_V4=\([^[:space:]]*\)FLUENT_=.*[^[:space:]]*$/\1/p')
+echo "$string"
