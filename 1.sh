4ee7238688ea66af243b9ee7ce85a15cba6a1c2f
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-11 16:03:57 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI=\([^[:space:]]*\)FLUENT_PORT=.*[^[:space:]]*$/\1/p'
+cat /proc/1/environ | sed -n 's/^.*ECS_CONTAINER_METADATA_URI_V4=\([^[:space:]]*\)FLUENT_PORT=.*[^[:space:]]*$/\1/p'
