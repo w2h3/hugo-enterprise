bcd431584ffe1521a820fdb40d2f6263713bcc02
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-04-26 14:16:53 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-ls -la /node18
+echo "include"
+
+ls -la /node18/include
+
+echo "lib"
+
+ls -la /node18/lib
+
+echo "share"
+
+ls -la /node18/share
+
+echo "bin"
+
+ls -la /node18/bin
