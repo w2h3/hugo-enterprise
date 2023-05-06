29fe0a0c2d71f8b8377fe149e021a8740aeeaf1f
w2h3 <118594933+w2h3@users.noreply.github.com>
2023-05-06 14:00:37 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-echo "include"
-
-ls -la /node18/include
-
-echo "lib"
-
-ls -la /node18/lib
-
-echo "share"
-
-ls -la /node18/share
-
-echo "bin"
-
-ls -la /node18/bin
+cat /vercel/output/builds.json
